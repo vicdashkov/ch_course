9020535a2fcaa5caaa1820e1f2b2da7b7e22f033
viktordashkov <vic.dashkov@gmail.com>
2018-12-16 16:29:07 -0800

    backup for configs fresh install notes for adding user cleanup docker-compose

--- snippets.sh
+++ snippets.sh
@@ -2,4 +2,10 @@
 docker exec -it some-clickhouse-server /bin/bash
 docker exec -it src_ch_1 /bin/bash
 docker-compose run --rm client --host ch
-docker run -it --rm yandex/clickhouse-client --host localhost:9009
+docker-compose run --rm client --host ch -u vic --password 12345 --database=pokemon
+docker run -it --rm yandex/clickhouse-client --host 0.0.0.0
+
+apt-get update && \
+apt-get install apt-file && \
+apt-file update && \
+apt-get install vim
