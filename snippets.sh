c08c83387510cb917549059ef0899cab4c032791
viktordashkov <vic.dashkov@gmail.com>
2018-12-26 11:30:19 -0800

    configs for cluster+zookeper; minor

--- snippets.sh
+++ snippets.sh
@@ -2,7 +2,7 @@
 docker exec -it some-clickhouse-server /bin/bash
 docker exec -it src_ch_1 /bin/bash
 docker-compose run --rm client --host ch1
-docker-compose run --rm client --host ch3 -u vic --password 12345 --database=pokemon
+docker-compose run --rm client --host ch4 -u vic --password 12345 --database=pokemon
 docker run -it --rm yandex/clickhouse-client --host 0.0.0.0
 
 apt-get update && \
