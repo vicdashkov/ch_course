e2ae61f22902e490e326524ab589d7d0a7ca68af
vdashkov <vdashkov@news360.com>
2019-02-24 13:54:13 -0800

    added files for merge tree overview

--- course_files/merge_tree_family/benc_merge_trees_insert.sh
+++ course_files/merge_tree_family/benc_merge_trees_insert.sh
@@ -1,37 +1,43 @@
 #!/usr/bin/env bash
 
 export TABLE_NAME=event_time_batch
-export BULK_SIZE=10
+export BULK_SIZE=100000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=0
+export EVENTS_PER_DAY=1000000
 python inserter.py
 
 export TABLE_NAME=event_time_single
-export BULK_SIZE=1
+export BULK_SIZE=1000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=0
+export EVENTS_PER_DAY=1000000
 python inserter.py
 
 export TABLE_NAME=even_time_order_func_batch
-export BULK_SIZE=10
+export BULK_SIZE=100000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=0
+export EVENTS_PER_DAY=1000000
 python inserter.py
 
 export TABLE_NAME=even_time_order_func_single
-export BULK_SIZE=1
+export BULK_SIZE=1000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=0
+export EVENTS_PER_DAY=1000000
 python inserter.py
 
 export TABLE_NAME=event_date_batch
-export BULK_SIZE=10
+export BULK_SIZE=100000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=1
+export EVENTS_PER_DAY=1000000
 python inserter.py
 
 export TABLE_NAME=event_date_single
-export BULK_SIZE=1
+export BULK_SIZE=1000
 export DB_NAME=merge_tree
 export HAS_DATE_COLUMN=1
+export EVENTS_PER_DAY=1000000
 python inserter.py
