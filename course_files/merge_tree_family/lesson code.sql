78354276ea2719aa29edabfe9bb07f0381074b26
vdashkov <vic.dashkov@gmail.com>
2019-03-02 13:49:54 -0800

    changes to lesson code

--- course_files/merge_tree_family/lesson code.sql
+++ course_files/merge_tree_family/lesson code.sql
@@ -10,22 +10,55 @@ CREATE TABLE merge_tree.event
 )
 ENGINE = MergeTree(); -- this won’t work
 
--- tab 2 in experiment_tables.sql
+-- tab 2 in
+CREATE TABLE merge_tree.event_time
+(
+  id UInt64,
+  time DateTime,
+  type UInt16,
+  pokemon_id UInt16
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(time)
+ORDER BY (time, id);
+-- pay attention to order of fields
+
+CREATE TABLE merge_tree.event_time_order_func
+(
+  id UInt64,
+  time DateTime,
+  type UInt16,
+  pokemon_id UInt16
+)
+ENGINE = MergeTree()
+PARTITION BY toYYYYMM(time)
+ORDER BY (toYYYYMM(time), id);
+
+CREATE TABLE merge_tree.event_date
+(
+  id UInt64,
+  time DateTime,
+  date Date,
+  type UInt16,
+  pokemon_id UInt16
+)
+ENGINE = MergeTree()
+PARTITION BY date
+ORDER BY (date, id);
 
 -- tab 3
-SELECT table, formatReadableSize(size) as size, rows, days, formatReadableSize(avgDaySize) as avgDaySize FROM (
+SELECT table, formatReadableSize(size) as size, rows FROM (
     SELECT
         table,
         sum(bytes) AS size,
         sum(rows) AS rows,
         min(min_date) AS min_date,
-        max(max_date) AS max_date,
-        (max_date - min_date) AS days,
-        size / (max_date - min_date) AS avgDaySize
+        max(max_date) AS max_date
     FROM system.parts
     WHERE active
     GROUP BY table
     ORDER BY rows DESC
 )
 
+-- tab 4 in experiment_tables.sql
 
