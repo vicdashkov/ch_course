f37c4c99757935a56df9ff441f1dff5e6f348166
vdashkov <vic.dashkov@gmail.com>
2019-03-09 07:23:33 -0800

    fixed agg for lesson on aggregates

--- course_files/aggregates/lesson code.sql
+++ course_files/aggregates/lesson code.sql
@@ -14,15 +14,16 @@ ORDER BY (time, id);
 
 CREATE MATERIALIZED VIEW aggregating.event_aggs
 ENGINE = AggregatingMergeTree()
-PARTITION BY toYYYYMM(time)
-ORDER BY (time, id)
+PARTITION BY toYYYYMM(hour)
+ORDER BY (hour, pokemon_id)
 AS SELECT
-    id,
-    time,
-    sumState(type)    AS types_sum,
-    uniqState(pokemon_id) AS unique_id
+    pokemon_id,
+    toStartOfHour(time) hour,
+    sumState(type) AS types_sum,
+    uniqState(type) AS unique_type,
+    sumIfState(type, pokemon_id % 2 = 0) as why_would_i_need_this
 FROM aggregating.event
-GROUP BY id, time;
+GROUP BY toStartOfHour(time), pokemon_id;
 
 -- tab 2
 INSERT INTO aggregating.event values(2, '2018-01-02 00:00:00', 1, 1);
@@ -32,9 +33,10 @@ INSERT INTO aggregating.event values(2, '2018-01-02 00:00:00', 3, 2);
 INSERT INTO aggregating.event values(2, '2018-01-02 00:00:00', 4, 3);
 
 -- tab 3
-SELECT sum(type), uniq(pokemon_id) FROM aggregating.event
+SELECT sum(type), uniq(type), sumIf(type, pokemon_id % 2 = 0) FROM aggregating.event
 
 SELECT
     sumMerge(types_sum),
-    uniqMerge(unique_id)
+    uniqMerge(unique_type),
+    sumIfMerge(why_would_i_need_this)
 FROM aggregating.event_aggs
