74569d5b59ca9c77b88aed5d38ec21e913a0dd7e
vdashkov <vic.dashkov@gmail.com>
2019-03-09 16:40:28 -0800

    notes on cluster. misc fixes to other stuff

--- course_files/replication and sharding/lesson code.sql
+++ course_files/replication and sharding/lesson code.sql
@@ -1,1 +1,32 @@
 -- tab 1
+SELECT * FROM system.clusters
+
+-- tab 2
+CREATE DATABASE pokemon_cluster on cluster pokemon
+
+CREATE TABLE pokemon_cluster.event_local on cluster pokemon
+(
+  id UInt64,
+  time DateTime,
+  type UInt16,
+  pokemon_id UInt16
+)
+ENGINE = ReplicatedMergeTree('/clickhouse/tables/{shard}/event_local', '{replica}')
+PARTITION BY toYYYYMM(time)
+ORDER BY (toYYYYMM(time), id);
+
+CREATE TABLE pokemon_cluster.event_distributed on cluster pokemon
+(
+  id UInt64,
+  time DateTime,
+  type UInt16,
+  pokemon_id UInt16
+)
+ENGINE = Distributed(pokemon, pokemon_cluster, event_local, rand())
+
+-- tab 3
+-- we are connected to ch1
+INSERT INTO pokemon_cluster.event_local VALUES(122, '2018-01-01 00:00:00', 33, 2222)
+
+INSERT INTO pokemon_cluster.event_distributed VALUES(22222222, '2019-01-01 00:00:00', 2, 222)
+INSERT INTO pokemon_cluster.event_distributed VALUES(44444444, '2019-01-01 00:00:00', 2, 222)
